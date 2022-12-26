de1577c57f315bb66af4213fe54e0a3070942f41
贺春旸 <chunyang_he@139.com>
2022-12-26 13:03:03 +0800

    Add files via upload

--- schema/redis_monitor.sql
+++ schema/redis_monitor.sql
@@ -40,7 +40,7 @@ CREATE TABLE `redis_status` (
   `aof_enabled` int DEFAULT NULL,
   `mode` varchar(100) DEFAULT NULL,
   `cluster_enabled` int DEFAULT NULL,
-  `Seconds_Behind_Master` int DEFAULT NULL,
+  `Seconds_Behind_Master` varchar(100) DEFAULT NULL,
   `create_time` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `IX_h_d_p_c` (`host`,`tag`,`port`,`create_time`)
@@ -70,7 +70,7 @@ CREATE TABLE `redis_status_history` (
   `aof_enabled` int DEFAULT NULL,
   `mode` varchar(100) DEFAULT NULL,
   `cluster_enabled` int DEFAULT NULL,
-  `Seconds_Behind_Master` int DEFAULT NULL,
+  `Seconds_Behind_Master` varchar(100) DEFAULT NULL,
   `create_time` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `IX_h_d_p_c` (`host`,`tag`,`port`,`create_time`)
@@ -98,29 +98,6 @@ CREATE TABLE `redis_status_info` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `IX_i_d_p` (`host`,`tag`,`port`)
 ) ENGINE=InnoDB AUTO_INCREMENT=277 DEFAULT CHARSET=utf8mb3 COMMENT='Redis监控信息表';
-
-/*Table structure for table `redis_status_info_bak` */
-
-DROP TABLE IF EXISTS `redis_status_info_bak`;
-
-CREATE TABLE `redis_status_info_bak` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `host` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '输入被监控Redis的IP地址',
-  `tag` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '输入被监控Redis的主机信息',
-  `pwd` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL COMMENT '输入被监控Redis的密码',
-  `port` int DEFAULT NULL COMMENT '输入被监控Redis的端口号',
-  `monitor` tinyint DEFAULT '1' COMMENT '0为关闭监控;1为开启监控',
-  `send_mail` tinyint DEFAULT '1' COMMENT '0为关闭邮件报警;1为开启邮件报警',
-  `send_mail_to_list` varchar(255) DEFAULT NULL COMMENT '邮件人列表',
-  `send_weixin` tinyint DEFAULT '1' COMMENT '0为关闭微信报警;1为开启微信报警',
-  `send_weixin_to_list` varchar(100) DEFAULT NULL COMMENT '微信公众号',
-  `alarm_threads_running` tinyint DEFAULT NULL COMMENT '记录活动连接数告警信息，1为已记录',
-  `threshold_alarm_threads_running` int DEFAULT NULL COMMENT '设置连接数阀值',
-  `alarm_repl_status` tinyint DEFAULT NULL COMMENT '记录主从复制告警信息，1为记录主从状态，3为记录主从延迟状态',
-  `threshold_warning_repl_delay` int DEFAULT NULL COMMENT '设置主从复制延迟阀值',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `IX_i_d_p` (`host`,`tag`,`port`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb3 COMMENT='Redis监控信息表';
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
